074d3b5e46bb6c2e8916de49889d2ae174fbfaa7
p_pbbzhang <zhangbinbin@yuewen.com>
2023-03-01 10:32:06 +0800

    feat(doc): 使用github pages

--- docs/.vitepress/cache/deps/@theme_index.js
+++ docs/.vitepress/cache/deps/@theme_index.js
@@ -1,24 +1,24 @@
 // node_modules/.pnpm/vitepress@1.0.0-alpha.49/node_modules/vitepress/dist/client/theme-default/index.js
-import "/Users/nickyzhang/Project/megrez/node_modules/.pnpm/vitepress@1.0.0-alpha.49/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
-import "/Users/nickyzhang/Project/megrez/node_modules/.pnpm/vitepress@1.0.0-alpha.49/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
-import "/Users/nickyzhang/Project/megrez/node_modules/.pnpm/vitepress@1.0.0-alpha.49/node_modules/vitepress/dist/client/theme-default/styles/base.css";
-import "/Users/nickyzhang/Project/megrez/node_modules/.pnpm/vitepress@1.0.0-alpha.49/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
-import "/Users/nickyzhang/Project/megrez/node_modules/.pnpm/vitepress@1.0.0-alpha.49/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
-import "/Users/nickyzhang/Project/megrez/node_modules/.pnpm/vitepress@1.0.0-alpha.49/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
-import "/Users/nickyzhang/Project/megrez/node_modules/.pnpm/vitepress@1.0.0-alpha.49/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
-import "/Users/nickyzhang/Project/megrez/node_modules/.pnpm/vitepress@1.0.0-alpha.49/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
-import "/Users/nickyzhang/Project/megrez/node_modules/.pnpm/vitepress@1.0.0-alpha.49/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
-import VPBadge from "/Users/nickyzhang/Project/megrez/node_modules/.pnpm/vitepress@1.0.0-alpha.49/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
-import Layout from "/Users/nickyzhang/Project/megrez/node_modules/.pnpm/vitepress@1.0.0-alpha.49/node_modules/vitepress/dist/client/theme-default/Layout.vue";
-import NotFound from "/Users/nickyzhang/Project/megrez/node_modules/.pnpm/vitepress@1.0.0-alpha.49/node_modules/vitepress/dist/client/theme-default/NotFound.vue";
-import { default as default2 } from "/Users/nickyzhang/Project/megrez/node_modules/.pnpm/vitepress@1.0.0-alpha.49/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
-import { default as default3 } from "/Users/nickyzhang/Project/megrez/node_modules/.pnpm/vitepress@1.0.0-alpha.49/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
-import { default as default4 } from "/Users/nickyzhang/Project/megrez/node_modules/.pnpm/vitepress@1.0.0-alpha.49/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
-import { default as default5 } from "/Users/nickyzhang/Project/megrez/node_modules/.pnpm/vitepress@1.0.0-alpha.49/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
-import { default as default6 } from "/Users/nickyzhang/Project/megrez/node_modules/.pnpm/vitepress@1.0.0-alpha.49/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
-import { default as default7 } from "/Users/nickyzhang/Project/megrez/node_modules/.pnpm/vitepress@1.0.0-alpha.49/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
-import { default as default8 } from "/Users/nickyzhang/Project/megrez/node_modules/.pnpm/vitepress@1.0.0-alpha.49/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
-import { default as default9 } from "/Users/nickyzhang/Project/megrez/node_modules/.pnpm/vitepress@1.0.0-alpha.49/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
+import "/Users/zhangbinbin/Yuewen/megrez/node_modules/.pnpm/vitepress@1.0.0-alpha.49/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
+import "/Users/zhangbinbin/Yuewen/megrez/node_modules/.pnpm/vitepress@1.0.0-alpha.49/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
+import "/Users/zhangbinbin/Yuewen/megrez/node_modules/.pnpm/vitepress@1.0.0-alpha.49/node_modules/vitepress/dist/client/theme-default/styles/base.css";
+import "/Users/zhangbinbin/Yuewen/megrez/node_modules/.pnpm/vitepress@1.0.0-alpha.49/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
+import "/Users/zhangbinbin/Yuewen/megrez/node_modules/.pnpm/vitepress@1.0.0-alpha.49/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
+import "/Users/zhangbinbin/Yuewen/megrez/node_modules/.pnpm/vitepress@1.0.0-alpha.49/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
+import "/Users/zhangbinbin/Yuewen/megrez/node_modules/.pnpm/vitepress@1.0.0-alpha.49/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
+import "/Users/zhangbinbin/Yuewen/megrez/node_modules/.pnpm/vitepress@1.0.0-alpha.49/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
+import "/Users/zhangbinbin/Yuewen/megrez/node_modules/.pnpm/vitepress@1.0.0-alpha.49/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
+import VPBadge from "/Users/zhangbinbin/Yuewen/megrez/node_modules/.pnpm/vitepress@1.0.0-alpha.49/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
+import Layout from "/Users/zhangbinbin/Yuewen/megrez/node_modules/.pnpm/vitepress@1.0.0-alpha.49/node_modules/vitepress/dist/client/theme-default/Layout.vue";
+import NotFound from "/Users/zhangbinbin/Yuewen/megrez/node_modules/.pnpm/vitepress@1.0.0-alpha.49/node_modules/vitepress/dist/client/theme-default/NotFound.vue";
+import { default as default2 } from "/Users/zhangbinbin/Yuewen/megrez/node_modules/.pnpm/vitepress@1.0.0-alpha.49/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
+import { default as default3 } from "/Users/zhangbinbin/Yuewen/megrez/node_modules/.pnpm/vitepress@1.0.0-alpha.49/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
+import { default as default4 } from "/Users/zhangbinbin/Yuewen/megrez/node_modules/.pnpm/vitepress@1.0.0-alpha.49/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
+import { default as default5 } from "/Users/zhangbinbin/Yuewen/megrez/node_modules/.pnpm/vitepress@1.0.0-alpha.49/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
+import { default as default6 } from "/Users/zhangbinbin/Yuewen/megrez/node_modules/.pnpm/vitepress@1.0.0-alpha.49/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
+import { default as default7 } from "/Users/zhangbinbin/Yuewen/megrez/node_modules/.pnpm/vitepress@1.0.0-alpha.49/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
+import { default as default8 } from "/Users/zhangbinbin/Yuewen/megrez/node_modules/.pnpm/vitepress@1.0.0-alpha.49/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
+import { default as default9 } from "/Users/zhangbinbin/Yuewen/megrez/node_modules/.pnpm/vitepress@1.0.0-alpha.49/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
 var theme = {
   Layout,
   NotFound,
--- docs/components/card.md
+++ docs/components/card.md
@@ -1,12 +1,9 @@
 ## Button 按钮
 
-<ea-button>默认按钮</ea-button>
-<ea-button type="primary">默认按钮</ea-button>
-
 ::: details 显示代码
 
 ```html
-<ea-button>默认按钮</ea-button> <ea-button type="primary">默认按钮</ea-button>
+<button>默认按钮</button>
 ```
 
 :::
